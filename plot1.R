07ef334f2e304af4f2e2d7adee264ef05cc0de10
gaurav23-27-ctrl <you@example.com>
2026-04-02 15:30:27 +0000

    Add plot files

---
+++ plot1.R
@@ -0,0 +1,6 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+png("plot1.png", width=480, height=480)
+hist(data$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings="?", stringsAsFactors=FALSE)
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+data <- subset(data, Date == "2007-02-01" | Date == "2007-02-02")
+data$DateTime <- strptime(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S")
+png("plot2.png", width=480, height=480)
+plot(data$DateTime, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
